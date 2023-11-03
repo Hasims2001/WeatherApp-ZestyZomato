e004c4da7dda05de468c55973da5cec6038a1dd1
Mohammad Hasim Shaikh <hasims2001@gmail.com>
2023-11-03 20:59:37 +0530

    weather app done

---
+++ Level2/app/ZestyZomato.py
@@ -0,0 +1,92 @@
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from passlib.hash import pbkdf2_sha256
+import json
+import datetime
+import random
+
+app = Flask(__name__)
+app.secret_key = 'secret'
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///ZestyZomato.sqlite3"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+class Dishes(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), unique=True, nullable=False)
+    price = db.Column(db.Integer, nullable=False)
+    availability = db.Column(db.String(10), nullable=False)
+    store =  db.Column(db.String(10), nullable=False)
+    def __init__(self, name, price, availability, store): 
+        self.name = name
+        self.price = price
+        self.availability = availability  
+        self.store = store
+
+
+with app.app_context():
+    db.create_all()
+
+@app.route('/', methods=['GET'])
+def welcome():
+    return jsonify({'message': "Welcome to Zesty Zomato"})
+
+@app.route("/dishes", methods=['GET', "POST"])
+def dishes():
+    # post request
+    if(request.method == 'POST'):
+        dish = request.get_json()
+        if(dish['name'] and dish['price'] and dish['availability'] and dish['store']):
+            new_dish = Dishes(name=dish['name'], price=dish['price'], availability=dish['availability'], store=dish['store'])
+            db.session.add(new_dish)
+            db.session.commit()
+
+            return jsonify({'message': "dish added"})
+
+
+    # Get request
+    allDishes = Dishes.query.all()
+    dishes_list = []
+
+    for dish in allDishes:
+        dish_info = {
+            'id': dish.id,
+            'name': dish.name,
+            'price': dish.price,
+            'availability': dish.availability,
+            'store': dish.store
+        }
+        dishes_list.append(dish_info)
+
+    return jsonify({'message': "All Dishes", "Dishes": dishes_list})
+
+
+@app.route("/dish/<int:dish_id>", methods=['GET', 'PUT', 'PATCH', 'DELETE'])
+def paramDishes(dish_id):
+    dish = Dishes.query.get(int(dish_id))
+    if(dish):
+        if(request.method == 'GET'):
+            return jsonify({'message': f'Single Dish', 'Dish': dish})
+        
+        if(request.method == 'DELETE'):
+            db.session.delete(dish)
+            db.session.commit()
+            return jsonify({'message': f'Dish with ID {dish_id} has been deleted'})
+        
+        data = request.get_json()
+        if 'name' in data:
+            dish.name = data['name']
+        if 'price' in data:
+            dish.price = data['price']
+        if 'availability' in data:
+            dish.availability = data['availability']
+        if 'store' in data:
+            dish.store = data['store']
+        db.session.commit()
+        return jsonify({'message': f'Dish with ID {dish_id} has been updated'})
+    else:
+        return jsonify({'message': 'Dish not found'}) 
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ Level2/tests/test_ZestyZomato.py
@@ -0,0 +1,37 @@
+from app.ZestyZomato import app
+import pytest
+import json
+
+@pytest.fixture
+def client():
+    return app.test_client()
+
+def test_welcome(client):
+    response = client.get('/')
+    data = json.loads(response.data)
+    assert 'Welcome to Zesty Zomato' == data['message']
+
+
+def test_getDishes(client):
+    response = client.get('/dishes')
+    data = json.loads(response.data)
+    assert 'All Dishes' == data['message']
+
+def test_Dishes_edge_case(client):
+    Single_Dish = client.get('/dish/0')
+    single = json.loads(Single_Dish.data)
+    assert 'Dish not found' == single['message']
+
+    Single_Dish = client.put('/dish/0')
+    single = json.loads(Single_Dish.data)
+    assert 'Dish not found' == single['message']
+
+    Single_Dish = client.delete('/dish/0')
+    single = json.loads(Single_Dish.data)
+    assert 'Dish not found' == single['message']
+
+def test_getDish(client):
+    response = client.get('/dish/1')
+    data = json.loads(response.data)
+    assert 'Single Dish' == data['message']
+
